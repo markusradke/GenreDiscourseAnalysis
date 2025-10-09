994527a4e504e5bfcb4893060b422ed336d32c10
Markus Radke <mail@markus-radke.de>
2025-10-09 17:07:29 +0200

    fix gephi export for characters in node labels not allowed in XML

--- R/handle_io.R
+++ R/handle_io.R
@@ -3,14 +3,24 @@ export_graph_for_gephi_import <- function(graph, name) {
     id = c(1:igraph::vcount(graph)),
     label = igraph::V(graph)$name
   ) |>
-    dplyr::mutate(label = stringr::str_replace_all(.data$label, "&", "&amp;"))
+    dplyr::mutate(
+      label = stringr::str_replace_all(.data$label, "&", "&amp;"),
+      label = stringr::str_replace_all(.data$label, "<", "&lt;"),
+      label = stringr::str_replace_all(.data$label, ">", "&gt;"),
+      label = stringr::str_replace_all(.data$label, "\"", ""),
+      label = stringr::str_replace_all(.data$label, "'", "")
+    )
+  n_edges <- igraph::ecount(graph)
+  if (n_edges == 0) {
+    message(sprintf("Graph %s has no edges, skipping export.", name))
+    return(NULL)
+  }
   edges_df <- as.data.frame(igraph::get.edges(
     graph,
     c(1:igraph::ecount(graph))
   ))
   colnames(edges_df) <- c("source", "target")
   edges_weight <- igraph::E(graph)$weight
-
   rgexf::write.gexf(
     nodes = nodes_df,
     edges = edges_df,
