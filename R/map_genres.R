a87d5a3a6fb7062837a9aa6b0d65a6d2ee08535b
Markus Radke <mail@markus-radke.de>
2025-11-04 12:21:13 +0100

    map genres to two solutions with differing details for both plattforms

--- R/map_genres.R
+++ R/map_genres.R
@@ -1,60 +1,61 @@
+#' Map tracks to metagenres with probabilities and hard class
+#'
+#' This function is pure and returns a data.frame with one row per track,
+#' probability columns p_<metagenre> and a chosen metagenre column.
 map_metagenres <- function(long, metagenre_mapping) {
-  message("Joining metagenre mapping to long format data...")
-  long_with_metagenres <- long |>
-    dplyr::left_join(
-      metagenre_mapping,
-      by = "tag_name"
-    )
-  message("Calculating metagenre prevalences and probabilities...")
-  metagenre_preval <- get_metagenre_prevalences(long_with_metagenres)
-  all_metagenres <- unique(metagenre_mapping$metagenre)
-  message(
-    "Formatting metagenre probabilities dataframe and getting hard classes..."
-  )
-  metagenre_probs_df <- get_metagenre_probs_df(
-    metagenre_preval,
-    all_metagenres
-  )
-  metagenre_probs_df
+  long_with_meta <- join_metagenre_mapping(long, metagenre_mapping)
+  preval <- get_metagenre_prevalences(long_with_meta)
+  all_meta <- unique(metagenre_mapping$metagenre)
+  get_metagenre_probs_df(preval, all_meta)
 }
 
-get_metagenre_prevalences <- function(long_with_metagenres) {
-  total_track_counts <- long_with_metagenres |>
+join_metagenre_mapping <- function(long, mapping) {
+  dplyr::left_join(long, mapping, by = "tag_name")
+}
+
+compute_total_counts <- function(df) {
+  df |>
     dplyr::group_by(.data$track.s.id) |>
     dplyr::summarize(
       total_counts = sum(.data$tag_count, na.rm = TRUE),
       .groups = "drop"
     )
+}
 
-  metagenre_preval <- long_with_metagenres |>
+compute_meta_counts <- function(df) {
+  df |>
     dplyr::group_by(.data$track.s.id, .data$metagenre) |>
     dplyr::summarize(
       metagenre_counts = sum(.data$tag_count, na.rm = TRUE),
       .groups = "drop"
-    ) |>
-    dplyr::left_join(
-      total_track_counts,
-      by = "track.s.id"
-    ) |>
+    )
+}
+
+get_metagenre_prevalences <- function(long_with_metagenres) {
+  totals <- compute_total_counts(long_with_metagenres)
+  meta_counts <- compute_meta_counts(long_with_metagenres)
+  meta_counts |>
+    dplyr::left_join(totals, by = "track.s.id") |>
     dplyr::mutate(
-      metagenre_prevalence = .data$metagenre_counts / .data$total_counts
+      metagenre_prevalence = metagenre_counts / total_counts
     ) |>
     dplyr::arrange(.data$track.s.id, dplyr::desc(.data$metagenre_prevalence))
-  return(metagenre_preval)
 }
 
 get_metagenre_probs_df <- function(metagenre_prevalences, all_metagenres) {
   expected_cols <- get_expected_column_names(all_metagenres)
-  metagenre_probs <- pivot_metagenre_prev_wider(metagenre_prevalences)
-  metagenre_probs_full <- add_unmentioned_cols_zero(
-    metagenre_probs,
-    expected_cols
-  )
-  get_hard_classes(metagenre_probs_full, metagenre_prevalences)
+  probs_wide <- pivot_metagenre_prev_wider(metagenre_prevalences)
+  probs_full <- add_unmentioned_cols_zero(probs_wide, expected_cols)
+  probs_max <- add_max_prob_cols(probs_full)
+  get_hard_classes(probs_max, metagenre_prevalences)
+}
+
+sanitize_name <- function(x) {
+  gsub("[ &-]", "", x)
 }
 
 pivot_metagenre_prev_wider <- function(metagenre_prevalences) {
-  wide <- metagenre_prevalences |>
+  wide_p <- metagenre_prevalences |>
     dplyr::distinct(
       .data$track.s.id,
       .data$metagenre,
@@ -66,14 +67,27 @@ pivot_metagenre_prev_wider <- function(metagenre_prevalences) {
       values_from = "metagenre_prevalence",
       values_fill = 0
     )
-  colnames(wide) <- gsub(" ", "", colnames(wide))
+  wide_n <- metagenre_prevalences |>
+    dplyr::distinct(
+      .data$track.s.id,
+      .data$metagenre,
+      .data$metagenre_counts
+    ) |>
+    tidyr::pivot_wider(
+      names_from = "metagenre",
+      names_prefix = "n_",
+      values_from = "metagenre_counts",
+      values_fill = 0
+    )
+  wide <- dplyr::left_join(wide_p, wide_n, by = "track.s.id")
+  colnames(wide) <- sanitize_name(colnames(wide))
   wide
 }
 
 get_expected_column_names <- function(metagenres) {
   cols <- paste("p_", metagenres, sep = "")
-  cols <- gsub(" ", "", cols)
-  return(cols)
+  cols <- c(cols, (paste("n_", metagenres, sep = "")))
+  sanitize_name(cols)
 }
 
 add_unmentioned_cols_zero <- function(df, expected_cols) {
@@ -84,19 +98,19 @@ add_unmentioned_cols_zero <- function(df, expected_cols) {
   df
 }
 
-get_hard_classes <- function(metagenre_probs_df, metagenre_prevalences) {
-  sum_of_prevanlences <- metagenre_prevalences |>
+compute_sum_prevalence <- function(metagenre_prevalences) {
+  metagenre_prevalences |>
     dplyr::group_by(.data$metagenre) |>
     dplyr::summarize(
       total_prevalence = sum(.data$metagenre_prevalence, na.rm = TRUE),
       .groups = "drop"
     )
+}
 
-  hard_classes <- metagenre_prevalences |>
-    dplyr::left_join(
-      sum_of_prevanlences,
-      by = "metagenre"
-    ) |>
+get_hard_classes <- function(metagenre_probs_df, metagenre_prevalences) {
+  sum_prev <- compute_sum_prevalence(metagenre_prevalences)
+  hard <- metagenre_prevalences |>
+    dplyr::left_join(sum_prev, by = "metagenre") |>
     dplyr::group_by(.data$track.s.id) |>
     dplyr::arrange(
       dplyr::desc(.data$metagenre_prevalence),
@@ -105,11 +119,23 @@ get_hard_classes <- function(metagenre_probs_df, metagenre_prevalences) {
     ) |>
     dplyr::slice_head(n = 1) |>
     dplyr::select("track.s.id", "metagenre")
+  dplyr::left_join(metagenre_probs_df, hard, by = "track.s.id")
+}
 
-  metagenre_probs_with_hard <- metagenre_probs_df |>
-    dplyr::left_join(
-      hard_classes,
-      by = "track.s.id"
-    )
-  metagenre_probs_with_hard
+add_max_prob_cols <- function(df) {
+  p_cols <- colnames(df)[grep("^p_", colnames(df))]
+  df$p_max <- apply(
+    df[, p_cols],
+    1,
+    max,
+    na.rm = TRUE
+  )
+  n_cols <- colnames(df)[grep("^n_", colnames(df))]
+  df$n_max <- apply(
+    df[, n_cols],
+    1,
+    max,
+    na.rm = TRUE
+  )
+  df
 }
--- analysis/02_fold_metagenres.R
+++ analysis/02_fold_metagenres.R
@@ -2,10 +2,12 @@ rm(list = ls())
 devtools::load_all()
 
 min_n <- 1000
-step <- 20
-max_n <- 5000
-optimal_range <- c(20, 30)
+step <- 5
+max_n <- 15000
+optimal_range <- c(25, 30)
 
+# tune metagenres ----
+message("Tuning metagenres...")
 tune_mb <- tune_tree_folding(
   read_feather_with_lists("data/filtered_mb_long.feather"),
   readRDS("models/trees/MusicBrainz_graph.rds"),
@@ -25,5 +27,57 @@ tune_s <- tune_tree_folding(
   optimal_solution_range_n_metagenres = optimal_range
 )
 save_tuning(tune_s, "s")
+
+# map genres for solution in 25-30 and 10-15 ranges ----
+message("Mapping metagenres for solution ranges...")
+map_genres_solution_range <- function(long, tune, n_range) {
+  suggested_min_n <- tune$ginis |>
+    dplyr::filter(n_metagenres >= n_range[1], n_metagenres <= n_range[2]) |>
+    dplyr::arrange(weighted_gini, dplyr::desc(min_n)) |>
+    dplyr::slice(1) |>
+    dplyr::pull(min_n)
+  suggested <- tune$solutions[[as.character(suggested_min_n)]]
+  map_metagenres(long, suggested$mapping)
+}
+
+mb_25_30 <- map_genres_solution_range(
+  read_feather_with_lists("data/filtered_mb_long.feather"),
+  tune_mb,
+  c(25, 30)
+)
+save_feather_with_lists(
+  mb_25_30,
+  "models/metagenres/mb_metagenres_25_30.feather"
+)
+
+s_25_30 <- map_genres_solution_range(
+  read_feather_with_lists("data/filtered_s_long.feather"),
+  tune_s,
+  c(25, 30)
+)
+save_feather_with_lists(
+  s_25_30,
+  "models/metagenres/s_metagenres_25_30.feather"
+)
+
+mb_10_15 <- map_genres_solution_range(
+  read_feather_with_lists("data/filtered_mb_long.feather"),
+  tune_mb,
+  c(10, 15)
+)
+save_feather_with_lists(
+  mb_10_15,
+  "models/metagenres/mb_metagenres_10_15.feather"
+)
+
+s_10_15 <- map_genres_solution_range(
+  read_feather_with_lists("data/filtered_s_long.feather"),
+  tune_s,
+  c(10, 15)
+)
+save_feather_with_lists(
+  s_10_15,
+  "models/metagenres/s_metagenres_10_15.feather"
+)
 
 generate_report("03_metagenres")
