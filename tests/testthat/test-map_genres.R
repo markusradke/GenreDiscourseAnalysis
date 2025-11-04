43feb3edcd0718b97d6a4c0aacf25bd1f7a1bbf8
Markus Radke <mail@markus-radke.de>
2025-11-04 12:21:13 +0100

    map genres to two solutions with differing details for both plattforms

--- tests/testthat/test-map_genres.R
+++ tests/testthat/test-map_genres.R
@@ -12,18 +12,8 @@ test_that("map_metagenres maps genres correctly", {
     tag_count = c(10, 5, 8, 12, 3, 16)
   )
   metagenre_mapping <- data.frame(
-    tag_name = c(
-      "alternative rock",
-      "rock",
-      "classical",
-      "jazz"
-    ),
-    metagenre = c(
-      "rock",
-      "rock",
-      "classical",
-      "jazz"
-    )
+    tag_name = c("alternative rock", "rock", "classical", "jazz"),
+    metagenre = c("rock", "rock", "classical", "jazz")
   )
   expect_no_error({
     mapped <- map_metagenres(long, metagenre_mapping)
@@ -32,15 +22,25 @@ test_that("map_metagenres maps genres correctly", {
   expect_equal(mapped$metagenre[mapped$track.s.id == "1"], "rock")
   expect_equal(mapped$metagenre[mapped$track.s.id == "2"], "classical")
   expect_equal(mapped$metagenre[mapped$track.s.id == "3"], "rock")
-  # check that result also contains the probabilities for all metagenres
   expect_setequal(
     colnames(mapped),
-    c("track.s.id", "p_rock", "p_classical", "p_jazz", "metagenre")
+    c(
+      "track.s.id",
+      "p_rock",
+      "p_classical",
+      "p_jazz",
+      "n_rock",
+      "n_classical",
+      "n_jazz",
+      "n_max",
+      "p_max",
+      "metagenre"
+    )
   )
 })
 
 test_that("get_metagenre_prevalences calculates prevalences correctly", {
-  long_with_metagenres <- data.frame(
+  long_with_meta <- data.frame(
     track.s.id = c("1", "1", "2", "3", "3", "3"),
     metagenre = c(
       "rock",
@@ -53,31 +53,34 @@ test_that("get_metagenre_prevalences calculates prevalences correctly", {
     tag_count = c(10, 5, 8, 12, 3, 16)
   )
   expect_no_error({
-    prevalences <- get_metagenre_prevalences(long_with_metagenres)
+    prevalences <- get_metagenre_prevalences(long_with_meta)
   })
   expect_equal(nrow(prevalences), 6)
   expect_equal(
     prevalences$metagenre_prevalence[
-      prevalences$track.s.id == "1" & prevalences$metagenre == "rock"
+      prevalences$track.s.id == "1" &
+        prevalences$metagenre == "rock"
     ],
     10 / (10 + 5)
   )
   expect_equal(
     prevalences$metagenre_prevalence[
-      prevalences$track.s.id == "2" & prevalences$metagenre == "classical"
+      prevalences$track.s.id == "2" &
+        prevalences$metagenre == "classical"
     ],
     1
   )
   expect_equal(
     prevalences$metagenre_prevalence[
-      prevalences$track.s.id == "3" & prevalences$metagenre == "rock"
+      prevalences$track.s.id == "3" &
+        prevalences$metagenre == "rock"
     ],
     16 / (12 + 3 + 16)
   )
 })
 
 test_that("get_metagenre_probs_df returns correct format", {
-  metagenre_prevalences <- data.frame(
+  met_prevs <- data.frame(
     track.s.id = c("1", "1", "2", "3", "3", "3"),
     metagenre = c(
       "rock",
@@ -87,17 +90,21 @@ test_that("get_metagenre_probs_df returns correct format", {
       "classical music",
       "rock"
     ),
-    metagenre_prevalence = c(0.67, 0.33, 1.0, 0.4, 0.1, 0.5)
+    metagenre_prevalence = c(0.67, 0.33, 1.0, 0.4, 0.1, 0.5),
+    metagenre_counts = c(10, 5, 8, 12, 3, 15)
   )
-  all_metagenres <- c("rock", "classical music", "jazz", "pop")
+  all_meta <- c("rock", "classical music", "jazz", "pop")
   expect_no_error({
-    probs_df <- get_metagenre_probs_df(metagenre_prevalences, all_metagenres)
+    probs_df <- get_metagenre_probs_df(met_prevs, all_meta)
   })
   expect_setequal(
     colnames(probs_df),
     c(
       "track.s.id",
-      gsub(" ", "", paste("p_", all_metagenres, sep = "")),
+      gsub("[ &-]", "", paste("p_", all_meta, sep = "")),
+      gsub("[ &-]", "", paste("n_", all_meta, sep = "")),
+      "n_max",
+      "p_max",
       "metagenre"
     )
   )
@@ -105,41 +112,70 @@ test_that("get_metagenre_probs_df returns correct format", {
   expect_equal(probs_df$p_pop, c(0, 0, 0))
   expect_equal(probs_df$p_rock[probs_df$track.s.id == "1"], 0.67)
   expect_equal(
-    rowSums(probs_df[, grep("^p_", colnames(probs_df))]),
+    rowSums(probs_df[, grep("^p_(?!max)", colnames(probs_df), perl = TRUE)]),
     c(1, 1, 1)
   )
-  expect_equal(
-    probs_df$metagenre,
-    c("rock", "classical music", "rock")
-  )
+  expect_equal(probs_df$metagenre, c("rock", "classical music", "rock"))
 })
 
 test_that("add_unmentioned_cols_zero adds missing columns with zeros", {
   df <- data.frame(
     track.s.id = c("1", "2"),
     p_rock = c(0.7, 0.4),
-    p_jazz = c(0.3, 0.6)
+    p_jazz = c(0.3, 0.6),
+    n_rock = c(7, 4),
+    n_jazz = c(3, 6)
   )
-  expected_cols <- c("p_rock", "p_jazz", "p_classical", "p_pop")
+  expected_cols <- c(
+    "p_rock",
+    "p_jazz",
+    "p_classical",
+    "p_pop",
+    "n_rock",
+    "n_jazz",
+    "n_classical",
+    "n_pop"
+  )
   result <- add_unmentioned_cols_zero(df, expected_cols)
   expect_setequal(colnames(result), c(expected_cols, "track.s.id"))
   expect_equal(result$p_classical, c(0, 0))
   expect_equal(result$p_pop, c(0, 0))
+  expect_equal(result$n_classical, c(0, 0))
+  expect_equal(result$n_pop, c(0, 0))
 })
 
-test_that("get_hard_classes uses fewer weighted sum of tag_counts to break ties", {
-  metagenre_probs_df <- data.frame(
+test_that("get_hard_classes uses smaller total_prevalence to break ties", {
+  meta_probs <- data.frame(
     track.s.id = c("1", "2"),
     p_rock = c(0.5, 0),
     p_jazz = c(0.5, 0.5)
   )
-  metagenre_prevalences <- data.frame(
+  meta_prevs <- data.frame(
     track.s.id = c("1", "1", "2"),
     metagenre = c("rock", "jazz", "rock"),
     metagenre_prevalence = c(0.5, 0.5, 1),
-    tag_count = c(20, 20, 10) # jazz has lower overall tag_count sum
+    tag_count = c(20, 20, 10)
   )
-
-  out <- get_hard_classes(metagenre_probs_df, metagenre_prevalences)
+  out <- get_hard_classes(meta_probs, meta_prevs)
   expect_equal(out$metagenre[out$track.s.id == "1"], "jazz")
+})
+
+
+test_that("sanitize_name removes spaces, ampersands, and hyphens", {
+  names <- c("rock & roll", "classical-music", "pop music")
+  sanitized <- sanitize_name(names)
+  expect_equal(sanitized, c("rockroll", "classicalmusic", "popmusic"))
+})
+
+test_that("add_max_prob_cols adds correct max probability columns", {
+  df <- data.frame(
+    track.s.id = c("1", "2"),
+    p_rock = c(0.8, 0.2),
+    p_jazz = c(0.2, 0.8),
+    n_rock = c(8, 2),
+    n_jazz = c(2, 8)
+  )
+  result <- add_max_prob_cols(df)
+  expect_equal(result$p_max, c(0.8, 0.8))
+  expect_equal(result$n_max, c(8, 8))
 })
