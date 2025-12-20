0183a719bc234a6ed1358080e647dc6e5f5f921e
= <=>
2025-12-20 16:53:03 +0100

    update dc contigency table

--- R/plot_dc_contingency.R
+++ R/plot_dc_contingency.R
@@ -17,9 +17,13 @@ plot_dc_contingency <- function(mb_meta, dc_genres, name = "COMGET") {
     tidyr::complete(
       metagenre,
       album.dc.firstgenre,
-      fill = list(Freq = 0, relfreq = 0, labelcolor = "black")
+      fill = list(
+        Freq = 0,
+        relfreq = 0,
+        relfreq_label = 0,
+        labelcolor = "black"
+      )
     )
-
   build_plot_dc_contingency(padded, no_metagenres, name)
 }
 
@@ -38,7 +42,10 @@ compute_relfreqs <- function(comb) {
     dplyr::group_by(metagenre, album.dc.firstgenre) |>
     dplyr::tally(name = "Freq") |>
     dplyr::group_by(metagenre) |>
-    dplyr::mutate(relfreq = Freq / sum(Freq)) |>
+    dplyr::mutate(
+      relfreq = Freq / sum(Freq),
+      relfreq_label = round(relfreq * 100, 0)
+    ) |>
     dplyr::ungroup() |>
     dplyr::mutate(
       labelcolor = dplyr::case_when(
@@ -59,14 +66,15 @@ build_plot_dc_contingency <- function(padded, no_metagenres, name = "COMGET") {
   ) +
     ggplot2::geom_tile() +
     ggplot2::geom_text(
-      ggplot2::aes(label = Freq, color = labelcolor),
-      show.legend = FALSE
+      ggplot2::aes(label = relfreq_label, color = labelcolor),
+      show.legend = FALSE,
+      size = 14 / ggplot2::.pt
     ) +
     ggplot2::scale_fill_gradient(
       low = "white",
-      high = "#c40d20",
+      high = "#3e578e",
       labels = scales::percent_format(accuracy = 1),
-      name = sprintf("Rel. freq\n(Discogs | %s-%d)", name, no_metagenres)
+      name = "",
     ) +
     ggplot2::scale_color_manual(values = c("black", "white")) +
     ggplot2::scale_x_discrete(position = "top") +
@@ -77,23 +85,23 @@ build_plot_dc_contingency <- function(padded, no_metagenres, name = "COMGET") {
     ggplot2::theme_minimal() +
     ggplot2::theme(
       legend.position = "right",
-      legend.text = ggplot2::element_text(color = "grey45"),
-      legend.title = ggplot2::element_text(color = "grey45"),
+      legend.text = ggplot2::element_text(color = "grey45", size = 16),
+      legend.title = ggplot2::element_text(color = "grey45", size = 16),
       axis.text.x = ggplot2::element_text(
-        angle = 45,
+        angle = 65,
         hjust = 0,
         vjust = 0,
-        size = 12
+        size = 16
       ),
-      axis.text.y = ggplot2::element_text(size = 12),
+      axis.text.y = ggplot2::element_text(size = 16),
       axis.title.x = ggplot2::element_text(
         color = "grey45",
-        size = 14,
+        size = 18,
         hjust = 0
       ),
       axis.title.y = ggplot2::element_text(
         color = "grey45",
-        size = 14,
+        size = 18,
         hjust = 1
       )
     )
